210db404a27faab8607c0fe25494e4f7597b203e
johanngan <johanngan.us@gmail.com>
2020-06-22 18:28:28 -0500

    Add utilities + fix unsigned->signed conversion

--- utils/memoryrange.lua
+++ utils/memoryrange.lua
@@ -1,9 +1,8 @@
--- Extra utilities for reading byte ranges
+-- Extra utilities for reading bytes
 memoryrange = {}
 
--- Reads a little-endian byte range and converts it to an unsigned int
-function memoryrange.readbytesUnsigned(address, length)
-    local bytes = memory.readbyterange(address, length)
+-- Converts a little-endian byte range to an unsigned int
+function memoryrange.bytesToUnsigned(bytes)
     local val = 0
     for i = #bytes, 1, -1 do
         val = 256*val + bytes[i]
@@ -11,12 +10,26 @@ function memoryrange.readbytesUnsigned(address, length)
     return val
 end
 
+-- Converts an n-byte unsigned to signed, assuming the last bit is the sign bit
+function memoryrange.unsignedToSigned(unsigned, n)
+    local lastbit = 2^(8*n - 1)
+    local signed = AND(lastbit - 1, unsigned)
+    return signed - math.abs(AND(lastbit, unsigned))
+end
+
+-- Converts a little-endian byte range to a signed int
+function memoryrange.bytesToSigned(bytes)
+    return memoryrange.unsignedToSigned(memoryrange.bytesToUnsigned(bytes), #bytes)
+end
+
+-- Reads a little-endian byte range and converts it to an unsigned int
+function memoryrange.readbytesUnsigned(address, length)
+    return memoryrange.bytesToUnsigned(memory.readbyterange(address, length))
+end
+
 -- Reads a little-endian byte range and converts it to a signed int
 function memoryrange.readbytesSigned(address, length)
-    local unsigned = memoryrange.readbytesUnsigned(address, length)
-    local lastbit = 2^(8*length - 1)
-    local signed = AND(lastbit - 1, unsigned)
-    return (AND(lastbit, unsigned) == 0) and signed or -signed
+    return memoryrange.bytesToSigned(memory.readbyterange(address, length), length)
 end
 
 return memoryrange
