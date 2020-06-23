b314b2eb318ab7e0c78c7b18cedec81257afbb7c
johanngan <johanngan.us@gmail.com>
2020-06-23 16:22:14 -0500

    NFC: reformatting

--- codes/direction.lua
+++ codes/direction.lua
@@ -5,7 +5,13 @@ if codes == nil then
 end
 
 -- Enum for directions (matches the internal direction index in-game)
-codes.DIRECTION, codes.DIRECTION_NAMES = enum.register(
-    {'Down', 'DownRight', 'Right', 'UpRight', 'Up', 'UpLeft', 'Left', 'DownLeft'},
-    0, 'direction'
-)
+codes.DIRECTION, codes.DIRECTION_NAMES = enum.register({
+    'Down',
+    'DownRight',
+    'Right',
+    'UpRight',
+    'Up',
+    'UpLeft',
+    'Left',
+    'DownLeft',
+}, 0, 'direction')
